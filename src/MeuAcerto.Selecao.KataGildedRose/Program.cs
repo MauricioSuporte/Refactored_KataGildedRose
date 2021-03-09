f05f115d197a7648142b84a4c88af55f484bcfbd
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-09 16:09:07 -0400

    Move Instanciamento de Itens para Repository

--- src/MeuAcerto.Selecao.KataGildedRose/Program.cs
+++ src/MeuAcerto.Selecao.KataGildedRose/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MeuAcerto.Selecao.KataGildedRose.Repository;
 
 namespace MeuAcerto.Selecao.KataGildedRose
 {
@@ -7,33 +8,8 @@ namespace MeuAcerto.Selecao.KataGildedRose
 	{
 		public static void Main(string[] args)
 		{
-			IList<Item> itens = new List<Item>{
-				new Item {Nome = "Corselete +5 DEX", PrazoParaVenda = 10, Qualidade = 20},
-				new Item {Nome = "Queijo Brie Envelhecido", PrazoParaVenda = 2, Qualidade = 0},
-				new Item {Nome = "Elixir do Mangusto", PrazoParaVenda = 5, Qualidade = 7},
-				new Item {Nome = "Dente do Tarrasque", PrazoParaVenda = 0, Qualidade = 80},
-				new Item {Nome = "Dente do Tarrasque", PrazoParaVenda = -1, Qualidade = 80},
-				new Item
-				{
-					Nome = "Ingressos para o concerto do Turisas",
-					PrazoParaVenda = 15,
-					Qualidade = 20
-				},
-				new Item
-				{
-					Nome = "Ingressos para o concerto do Turisas",
-					PrazoParaVenda = 10,
-					Qualidade = 49
-				},
-				new Item
-				{
-					Nome = "Ingressos para o concerto do Turisas",
-					PrazoParaVenda = 5,
-					Qualidade = 49
-				},
-				// Este item conjurado ainda não funciona direto!
-				new Item {Nome = "Bolo de Mana Conjurado", PrazoParaVenda = 3, Qualidade = 6}
-			};
+			var repositorio = new Repositorio();
+			IList<Item> itens = repositorio.get();
 
 			var app = new GildedRose(itens);
 
---
+++ src/MeuAcerto.Selecao.KataGildedRose/Repository/Repositorio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeuAcerto.Selecao.KataGildedRose.Repository
+{
+    public class Repositorio
+    {
+		public IList<Item> get()
+        {
+			IList<Item> itens = new List<Item>{
+				new Item {Nome = "Corselete +5 DEX", PrazoParaVenda = 10, Qualidade = 20},
+				new Item {Nome = "Queijo Brie Envelhecido", PrazoParaVenda = 2, Qualidade = 0},
+				new Item {Nome = "Elixir do Mangusto", PrazoParaVenda = 5, Qualidade = 7},
+				new Item {Nome = "Dente do Tarrasque", PrazoParaVenda = 0, Qualidade = 80},
+				new Item {Nome = "Dente do Tarrasque", PrazoParaVenda = -1, Qualidade = 80},
+				new Item
+				{
+					Nome = "Ingressos para o concerto do Turisas",
+					PrazoParaVenda = 15,
+					Qualidade = 20
+				},
+				new Item
+				{
+					Nome = "Ingressos para o concerto do Turisas",
+					PrazoParaVenda = 10,
+					Qualidade = 49
+				},
+				new Item
+				{
+					Nome = "Ingressos para o concerto do Turisas",
+					PrazoParaVenda = 5,
+					Qualidade = 49
+				},
+				new Item {Nome = "Bolo de Mana Conjurado", PrazoParaVenda = 3, Qualidade = 6}
+			};
+
+			return itens;
+		}
+	}
+}
