eb8c9312c4b9de34e2dde07b53dfc091954dad76
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-09 20:31:52 -0400

    Adiciona Enum TipoItem

---
+++ src/MeuAcerto.Selecao.KataGildedRose/Enum/TipoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeuAcerto.Selecao.KataGildedRose.Enum
+{
+    public enum TipoItem
+    {
+        ItemComum,
+        QueijoBrie,
+        Ingressos,
+        Lendario,
+        Conjurado,
+    }
+}
