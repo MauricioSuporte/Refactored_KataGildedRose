14289f92b09a2bc2716d60ab54f971b44dbcd996
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-11 13:14:16 -0400

    Altera Startup para iniciar mostrando API Itens

--- src/MeuAcerto.Selecao.KataGildedRose/Startup.cs
+++ src/MeuAcerto.Selecao.KataGildedRose/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,21 +14,36 @@ namespace MeuAcerto.Selecao.KataGildedRose
 {
     public class Startup
     {
-        public void ConfigurationServices(IServiceCollection service)
+        public Startup(IConfiguration configuration)
         {
-
+            Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
+
+        // Este método é chamado em tempo de execução. Usar este método para adicionar services para o container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // Este método é chamado em tempo de execução. Usar este método para configurar o HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Ola web");
-                });
+                endpoints.MapControllers();
             });
         }
     }
