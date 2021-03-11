1517abeb376d15b34f6e853768efbd0cc489b968
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-11 13:13:36 -0400

    Cria Controller GildedRose

---
+++ src/MeuAcerto.Selecao.KataGildedRose/Controllers/GildedRoseController.cs
@@ -0,0 +1,22 @@
+﻿using MeuAcerto.Selecao.KataGildedRose.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuAcerto.Selecao.KataGildedRose.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class GildedRoseController : ControllerBase
+    {
+        public List<Item> Index()
+        {
+            var repositorio = new Repositorio();
+            IList<Item> itens = repositorio.GetItens();
+            return (List<Item>)itens;
+        }
+    }
+}
