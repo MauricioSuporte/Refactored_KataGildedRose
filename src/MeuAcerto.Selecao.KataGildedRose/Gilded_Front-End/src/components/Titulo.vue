e1ec570581025bd0cc9ec0f2fceefd63a0f7804a
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-12 19:11:12 -0400

    Adiciona componente Titulo.vue

---
+++ src/MeuAcerto.Selecao.KataGildedRose/Gilded_Front-End/src/components/Titulo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="titulo">
+        <h1>{{titulo}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Titulo',
+  props: {
+      titulo: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #F5EF10;
+  font-size: 100px;
+  text-align: center;
+  font-family: Brush Script MT;
+}
+</style>
