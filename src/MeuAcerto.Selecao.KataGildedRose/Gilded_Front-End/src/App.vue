881201063a020ca400fa4115ef4e5747ffbc70bc
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-12 14:26:03 -0400

    Coleta dados da API e coloca no Front-End

---
+++ src/MeuAcerto.Selecao.KataGildedRose/Gilded_Front-End/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Gilded logo" src="./assets/logo.png">
+    <Itens />
+  </div>
+</template>
+
+<script>
+import Itens from './components/Itens.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Itens
+  }
+}
+</script>
---
+++ src/MeuAcerto.Selecao.KataGildedRose/Gilded_Front-End/src/components/Itens.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Nome
+                    </th>
+                    <th>
+                        Prazo Para Venda
+                    </th>
+                    <th>
+                        Qualidade
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in itens" v-bind:key="item.nome">
+                    <td>
+                        {{item.nome}}
+                    </td>
+                    <td>
+                        {{item.prazoParaVenda}}
+                    </td>
+                    <td>
+                        {{item.qualidade}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Itens',
+  data: () => {
+    return {
+      itens: [],
+    }
+  },
+  methods: {
+    lista: (scope) => {
+      axios.get(`https://localhost:44336/api`).then((res)=> {
+        console.log(res)
+        scope.itens = res.data
+      })
+    }
+  },
+  created() {
+    this.lista(this)
+  }
+}
+</script>
