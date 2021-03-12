d53027cd572cde9e148d42e87b1a36232bff3531
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-12 19:11:31 -0400

    Adiciona Componente Footer.vue

---
+++ src/MeuAcerto.Selecao.KataGildedRose/Gilded_Front-End/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="footer">
+        <span>💻 Made by Maurício Suporte</span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+      footer: String
+  }
+}
+</script>
+
+<style scoped>
+span {
+  color: #fff;
+  font-size: 20px;
+  text-align: center;
+  background-color: #333;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+</style>
