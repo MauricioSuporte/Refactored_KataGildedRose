28c14476fb6a30cdc17519c556a32e4f69f0f091
Mauricio Suporte <mauricio.souza@aluno.ic.ufmt.br>
2021-03-12 23:15:08 -0400

    Implementa método atualizar Itens na controller

--- src/MeuAcerto.Selecao.KataGildedRose/Controllers/GildedRoseController.cs
+++ src/MeuAcerto.Selecao.KataGildedRose/Controllers/GildedRoseController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace KataGildedRose.Controllers
 {
-    [Route("api")]
+    [Route("Api")]
     [ApiController]
     public class GildedRoseController : ControllerBase
     {
@@ -18,25 +18,15 @@ namespace KataGildedRose.Controllers
         {
             var repositorio = new Repositorio();
             List<Item> itens = repositorio.GetItens().ToList();
+            return itens;
+        }
 
-            //Dictionary<int, IList<Item>> novosItens = new Dictionary<int, IList<Item>>();
-
-            //var gildedRose = new GildedRose(itens);
-
-            //for (var i = 0; i < 31; i++)
-            //{
-            //    Console.WriteLine("-------- dia " + i + " --------");
-            //    Console.WriteLine("Nome, PrazoParaVenda, Qualidade");
-            //    for (var j = 0; j < itens.Count; j++)
-            //    {
-            //        Console.WriteLine(itens[j].Nome + ", " + itens[j].PrazoParaVenda + ", " + itens[j].Qualidade);
-            //    }
-            //    Console.WriteLine("");
-            //    gildedRose.AtualizarQualidade();
-            //}
-
-            
-            //string json = JsonConvert.SerializeObject(novosItens, Formatting.Indented);
+        [HttpPost]
+        [Route("Att")]
+        public List<Item> AtualizarItens(List<Item> itens)
+        {
+            var gildedRoseService = new GildedRose(itens);
+            gildedRoseService.AtualizarQualidade();
             return itens;
         }
     }
